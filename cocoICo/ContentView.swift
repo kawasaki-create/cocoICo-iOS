fe8011bd4373ae82a3cfe5ccef05dbf40ceebda3
kawasaki <r.kawasaki.biz@gmail.com>
2022-09-24 01:07:05 +0900

    モーダルのガワだけ作成

--- cocoICo/ContentView.swift
+++ cocoICo/ContentView.swift
@@ -10,30 +10,74 @@ import SwiftUI
 struct ContentView: View {
     @State var txt = "こんにちは"
     @State var img = ""
+    @State var showingModal = false
     
     var body: some View {
-                NavigationView {
+            NavigationView {
+                ZStack{
+                    Color(red:0.753, green: 1, blue: 0.778)
+                        .ignoresSafeArea()
                     VStack {
-                        NavigationLink(destination: NextView()) {
-                            Text("クリックで遷移") .padding()
-                                .navigationTitle("FirstTitle")
-                                .navigationBarTitleDisplayMode(.inline)
+                            Rectangle()
+                                .fill(Color.white)
+                                .ignoresSafeArea()
+                                .frame(maxHeight: 300)
+                                .overlay(Text("cocoICo.")
+                                    .font(.largeTitle)
+                                    .fontWeight(.bold)
+                                    )
+                        Text("").padding(30)
+                        Button("登録・ログイン") {
+                            self.showingModal.toggle()
                         }
-                        Text(txt + "\n")
-                            .padding()
-                        Button("適当ボタン") {
-                            txt = "foo"
+                        .sheet(isPresented: $showingModal){
+                            NextView()
                         }
+                        .padding(20)
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 2)
+                        )
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
+                        .navigationTitle("FirstTitle")
+                        .navigationBarTitleDisplayMode(.inline)
                     }
+            }
                 }
     }
 }
 
 struct NextView: View{
+    @State var email = ""
+    @State var password = ""
     var body: some View{
-        Text("NextView").padding()
-            .navigationBarTitle("SecontScreen")
-            .navigationBarTitleDisplayMode(.automatic)
+        VStack{
+            TextField("メールアドレス\n", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("パスワード\n", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            HStack(spacing:100){
+                Button("登録") {
+                    
+                }
+                .font(.headline)
+                .foregroundColor(Color.white)
+                .padding(EdgeInsets(
+                    top: 20, leading: 35, bottom: 20, trailing: 35
+                ))
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 1)
+                )
+                .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
+                Button("ログイン") {
+                    
+                }
+                .font(.headline)
+                .foregroundColor(Color.white)
+                .padding(20)
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 1)
+                )
+                .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
+            }
+            .padding(.top, 20)
+        }
     }
 }
 
